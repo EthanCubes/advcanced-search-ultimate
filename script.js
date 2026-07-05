f67c39e45698d55dc91ad6f2f9526d1241cb5f9d
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-04 18:51:46 -0600

    Added option to select search engines.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let query;
 let link;
+let engine;
+
+const engineIntro = {"DuckDuckGo": "https://www.duckduckgo.com/?=",
+    "StartPage": "https://www.startpage.com/do/metasearch.pl?query=", 
+    "Google": "https://www.google.com/search?hl=en&q="};
+
 const containAll = document.getElementById("containAll");
+const searchEngineSelect = document.getElementById("searchEngineSelect");
 
 function search() {
-    link = "https://www.duckduckgo.com/?q=" + containAll.value;
+    query = containAll.value;
+    link = engineIntro[engine] + query;
     window.open(link);
 }
+
+setInterval(function() {engine = searchEngineSelect.value}, 10)
