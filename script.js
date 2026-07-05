bb3aed72e3c7f47ce6b1f6d9e2b2cff4f1ed45ef
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-04 19:40:34 -0600

    Added code to integrate advanced search operators into query.

--- script.js
+++ script.js
@@ -2,15 +2,46 @@ let query;
 let link;
 let engine;
 
+let queryElements = {
+    all: [],
+    specific: "",
+    none: [],
+    site: "",
+};
+
 const engineIntro = {"DuckDuckGo": "https://www.duckduckgo.com/?=",
     "StartPage": "https://www.startpage.com/do/metasearch.pl?query=", 
     "Google": "https://www.google.com/search?hl=en&q="};
 
-const containAll = document.getElementById("containAll");
+const all = document.getElementById("all");
 const specific = document.getElementById("specific");
+const none = document.getElementById("none");
+const site = document.getElementById("site");
+
+function getQueryElements() {
+    queryElements.all = all.value.split(/\s+/);
+    queryElements.specific = specific.value;
+    queryElements.none = none.value.split(/\s+/);
+    queryElements.site = site.value;
+}
+
+function formQuery() {
+    query = "";
+    for (let i = 0; i < queryElements.all.length; i++) {
+        query += queryElements.all[i] + " ";
+    }
+    query += "\"" + queryElements.specific + "\" ";
+    for (let i = 0; i < queryElements.none.length; i++) {
+        query += "-" + queryElements.none[i] + " ";
+    }
+    if (!(queryElements.site === "")) {
+        query += "site:" + queryElements.site;
+    }
+}
 
 function search() {
-    query = containAll.value + ' "' + specific.value + '"';
+    getQueryElements();
+    formQuery();
     navigator.clipboard.writeText(query);
     alert("Search query copied to clipboard. Paste it into any search engine");
 }
